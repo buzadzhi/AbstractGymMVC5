3461782a4223c120461834e08c9ee71e5e03d2f3
Constantine <kyakimov25@gmail.com>
2017-09-17 10:35:27 -0400

    Initial State. - Basic Data Model created for appointment system. - CRUD functionality for ApointmentPerson has been implemented.

--- AbstractGymMVC5/Models/Appointment.cs
+++ AbstractGymMVC5/Models/Appointment.cs
@@ -7,7 +7,7 @@ namespace AbstractGymMVC5.Models
 {
     public class Appointment
     {
-        public int AppointmentID { get; set; }
+        public int AppointmentID { get; set; } //test
         public int RoomID { get; set; }
         public int AppointmentPersonID { get; set; }
         public DateTime AppointmentDate { get; set; }
